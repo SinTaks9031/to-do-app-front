4ce9c235a7f26daa8279b20daa9c096c04b2d8bf
takahaku <tkzhaku@gmail.com>
2024-12-09 17:53:09 +0200

    Change landing text

--- src/components/Layout/Landing.js
+++ src/components/Layout/Landing.js
@@ -17,7 +17,7 @@ class Landing extends Component {
             <div className="row">
               <div className="col-md-12 text-center">
                 <h1 className="display-3 mb-4">
-                  Personal Project Management Tool
+                  To Do List Management Tool
                 </h1>
                 <p className="lead">
                   Create your account to join active projects or start your own
